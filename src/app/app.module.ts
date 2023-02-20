67ed1704b4e20bc7cef319baf2d2fbe5e42ef401
davdemete <davdemeter@gmail.com>
2023-02-20 14:40:08 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from  HttpClientModule
 //Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
