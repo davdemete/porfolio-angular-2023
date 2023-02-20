fa5489c71138422b17e8121d32b0792fec243528
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-02-20 09:57:07 +0100

    year

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
+//Módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ItemComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
